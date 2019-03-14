49ba0bfb4336908e13f4b471a3ab01788991828b
David Nankivell <David.Nanki@gmail.com>
2019-03-14 19:57:11 +0800

    Various Commands

--- core/commands.js
+++ core/commands.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const disciplines = ["Programming", "Modelling", "Texturing", "Audio", "SourceControl"];
+let currentMessage = {};
 
 function processCommand(message, client) {
   //Remove command char, '!'
@@ -6,24 +8,28 @@ function processCommand(message, client) {
   let primaryCommand = command.split(" ")[0];
   let arguments = command.split(" ").slice(1);
 
-  if (message.channel.id === "516859846809616413") {
-    devCommands(arguments, message, client, primaryCommand);
-  }
+  currentMessage = message;
 
-  if (primaryCommand === 'help') {
-    helpCommand(arguments, message);
-  } else if (primaryCommand === 'multiply') {
-    multiplyCommand(arguments, message);
-  } else if (primaryCommand === 'role') {
-    roleCommand(arguments, message, client);
-  //} else if (/*removeRole*/) {
-    //removeRoleCommand()
-  } else if (primaryCommand === 'ideas') {
-    ideasCommand(arguments, message);
-  } else if (primaryCommand === 'listIdeas') {
-    listIdeasCommand(arguments, message, client);
-  } else {
-    message.channel.send(`Soz fren, don't understand that command! Pls ask for !help if you need it!`);
+  if (channel('bot-test')) {
+    devCommands(arguments, message, client, primaryCommand);
+  } else if (channel('bot-commands')) {
+    if ((primaryCommand === 'ideas')) {
+      ideasCommand(arguments, message);
+    } else if (primaryCommand === 'addResource') {
+      resourcesCommand(arguments, message);
+    } else if (primaryCommand === 'resources') {
+      listResourcesCommand(arguments, message);
+    } else if (primaryCommand === 'help') {
+      helpCommand(arguments, message);
+    } else if (primaryCommand === 'multiply') {
+      multiplyCommand(arguments, message);
+    } else if (primaryCommand === 'role') {
+      roleCommand(arguments, message, client);
+    } else if (primaryCommand === 'listIdeas') {
+      listIdeasCommand(arguments, message, client);
+    } else {
+      message.channel.send(`Soz fren, don't understand that command! Pls ask for !help if you need it!`);
+    }
   }
 }
 
@@ -33,6 +39,11 @@ function devCommands(arguments, message, client, primaryCommand) {
   }
 }
 
+function channel(target) {
+  if (target === 'bot-commands') return (currentMessage.channel.id === '551319956822687770');
+  else if (target === 'bot-test') return (currentMessage.channel.id === '516859846809616413');
+}
+
 function helpCommand(arguments, message) {
   if (arguments.length > 0) {
     //Give help for specific command
@@ -40,16 +51,19 @@ function helpCommand(arguments, message) {
       message.channel.send('To choose a role, enter `!role role`');
     } else if (arguments[0] === 'ideas') {
       message.channel.send('Type `!ideas This is my idea` so we can see what you think!');
-    } else if (arguments[0] === 'multiply') {
-      message.channel.send('Try `!multiply 2 4 10`');
+    } else if (arguments[0] === 'addResource') {
+      message.channel.send('Type `!addResource [area] [url]`\nAvailable areas are:\n```\n' + disciplines.join('\n') + '```\nFor example: `!addResource Programming google.com`');
+    } else if (arguments[0] === 'resources') {
+      message.channel.send('Type `!resource [area]` to list available resources\nThe [area] field is optional');
     }
   } else {
     message.channel.send(`These are the available commands:
-      !role - Allows users to manage their own roles
-      !ideas - Suggestion box for bot improvements!
-      !multiply - Multiplies given numbers and returns the results
-      ...
-      To get more help with these commands, enter '!help command'`);
+!role - Allows users to manage their own roles
+!ideas - Suggestion box for bot/club improvements!
+!addResource - Library of URLs to resources
+!resources - List resources in the library
+...
+To get more help with these commands, enter '!help command'`);
   }
 }
 
@@ -74,7 +88,6 @@ function roleCommand(arguments, message, client) {
     message.guild.roles.forEach(role => {
       //Only offer roles below bot's roles, and not the default (lowest) role
       if (role.position < user.highestRole.position && role.position != 0) {
-        console.log(role.name);
         availableRoles.push(role);
       }
     });
@@ -191,12 +204,179 @@ function listIdeasCommand(arguments, message, client) {
 
             let msg = 'Here are our member suggestions!\n';
             suggestionObject.suggestions.forEach(suggestion => {
-              msg += `Author: ${suggestion.user}\n`;
-              msg += `Suggestion:\n ${suggestion.suggestion}\n\n`;
+              msg += `__Author__: ${suggestion.user}\n`;
+              msg += `__Suggestion__:\n ${suggestion.suggestion}\n\n`;
             });
             message.channel.send(msg);
           });
         }
+      });
+    }
+  });
+}
+
+function resourcesCommand(arguments, message) {
+  if (arguments.length > 0) {
+
+    if (disciplines.includes(arguments[0])) {
+
+      let resourceObject = {};
+
+      const path = `./core/data/resources/${message.guild.name}.json`;
+
+      fs.access(path, err => {
+        if (err) {
+          //Initial resource file creation
+          resourceObject = {
+            resourceGroups: [
+              {
+                discipline: "Programming",
+                resources: []
+              },
+              {
+                discipline: "Modelling",
+                resources: []
+              },
+              {
+                discipline: "Texturing",
+                resources: []
+              },
+              {
+                discipline: "Audio",
+                resources: []
+              }
+            ]
+          };
+          fs.writeFile(path, JSON.stringify(resourceObject, null, 4), err => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(`${path} created`);
+            }
+          });
+        }
+
+        fs.readFile(path, 'utf8', (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            //Assuming the file exists, load its data
+            resourceObject = JSON.parse(data);
+            if (!arguments[1].includes('https://') && !arguments[1].includes('http://')) {
+              arguments[1] = `https://${arguments[1]}`;
+            }
+            //Load list of all resources in file to prevent duplicates
+            const fileResources = [];
+            resourceObject.resourceGroups.forEach(group => {
+              if (group.discipline === arguments[0]) {
+                group.resources.forEach(resource => {
+                  fileResources.push(resource.resource);
+                });
+              }
+            });
+
+            if (!fileResources.includes(arguments[1])) {
+              //Add the resource data
+              let resource = {
+                id: message.author.id,
+                user: message.member.displayName,
+                resource: arguments[1]
+              };
+              //Load a list of all disciplines in file
+              const fileDisciplines = [];
+              resourceObject.resourceGroups.forEach(group => {
+                fileDisciplines.push(group.discipline);
+              });
+              //Check if the requested discipline is in the file list. By this point, it is already a discipline
+              //in the array specified at start
+              if (!fileDisciplines.includes(arguments[0])) {
+                //Add the discipline group
+                resourceObject.resourceGroups.push({ discipline: arguments[0], resources: [] });
+              }
+              //Find the discipline group, and add the resource object
+              resourceObject.resourceGroups.forEach(group => {
+                if (group.discipline === arguments[0]) {
+                  group.resources.push(resource);
+                }
+              });
+              //Write resources to file
+              fs.writeFile(path, JSON.stringify(resourceObject, null, 4), (err) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+
+                message.channel.send(`Resource added to library! Thanks ${message.member.displayName}!`);
+              });
+            } else {
+              message.channel.send(`Thanks ${message.member.displayName}, but this resource is already in our library!`);
+            }
+          }
+        });
+      });
+    } else {
+      message.channel.send(`Hey ${message.member.displayName}, thanks for adding a resource!
+However there's been a small mistake; The second argument needs to be one of the following:
+${disciplines.join('\n')}
+Thanks!`);
+    }
+
+  } else {
+    message.channel.send(`Hey ${message.member.displayName}, thanks for adding a resource!
+However there's been a small mistake; please try again like this: '!addResource Programming stackoverflow.com'
+Thanks!`);
+  }
+}
+
+function listResourcesCommand(arguments, message) {
+  const path = `./core/data/resources/${message.guild.name}.json`;
+
+  //Check resource file exists
+  fs.access(path, err => {
+    if (err) {
+      console.log('Access File Error');
+      console.log(err);
+      message.channel.send(`Soz fren, no resources have been added!`);
+    } else {
+      fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          console.log('Read File Error');
+          console.log(err);
+        }
+
+        let resourceObject = JSON.parse(data);
+        //Construct a response message based on given discipline and file data
+        let msg = `These are posted ${arguments.length > 0 ? `${arguments[0]} ` : ''}resources!\n`;
+        if (arguments.length == 0) msg += 'You can filter these with `!resources [area]`\n';
+        resourceObject.resourceGroups.forEach(group => {
+          if (arguments.length > 0) {
+            if (disciplines.includes(arguments[0])) {
+              if (group.discipline === arguments[0]) {
+                //Print resource in given group
+                msg += `${group.discipline}\n`;
+                group.resources.forEach(resource => {
+                  msg += '<';
+                  msg += resource.resource;
+                  msg += '>\n';
+                });
+                msg += '\n';
+              }
+            } else {
+              msg += 'There are no resources of that type\n';
+            }
+          } else {
+            //Print all resources
+            msg += `${group.discipline}\n`;
+            group.resources.forEach(resource => {
+              msg += '<';
+              msg += resource.resource;
+              msg += '>\n';
+            });
+            msg += '\n';
+          }
+
+        });
+        message.channel.send(msg);
       });
     }
   });
